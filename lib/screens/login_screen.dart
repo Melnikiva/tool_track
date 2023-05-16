ea502f4f379452c9763dc1caa4e51cad7c77416a
Ivan Melnyk <ivanmel.vn23@gmail.com>
2023-05-16 14:27:28 +0300

    Login screen design

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tool_track/constants.dart';
+import 'package:tool_track/screens/assets_screen.dart';
 
 const double kFormElemetsGap = 16.0;
 
@@ -18,7 +20,6 @@ class LoginScreen extends StatelessWidget {
               child: Icon(
                 Icons.handyman_rounded,
                 size: 250.0,
-                color: Colors.blue.shade300,
               ),
             ),
             LoginForm(),
@@ -74,30 +75,75 @@ class LoginForm extends StatelessWidget {
           SizedBox(
             height: kFormElemetsGap * 2,
           ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FormButton(
+                formKey: _formKey,
+                text: 'Login',
+                onPressed: () {
                   // TODO: Login here
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(content: Text('Logging in...')),
                   );
-                  Navigator.pushNamed(context, '/assets');
-                }
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text('Login'),
+                  Navigator.pushNamed(context, AssetsScreen.route);
+                },
+                color: kSecondaryColor,
               ),
-              style: ButtonStyle(
-                textStyle: MaterialStatePropertyAll<TextStyle>(
-                  TextStyle(fontSize: 18.0),
-                ),
+              SizedBox(
+                height: 16.0,
               ),
-            ),
+              FormButton(
+                formKey: _formKey,
+                text: 'Register',
+                onPressed: () {
+                  // TODO: Register here
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Registering...')),
+                  );
+                  Navigator.pushNamed(context, AssetsScreen.route);
+                },
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class FormButton extends StatelessWidget {
+  const FormButton({
+    super.key,
+    required GlobalKey<FormState> formKey,
+    required this.text,
+    required this.onPressed,
+    this.color = kPrimaryColor,
+  }) : _formKey = formKey;
+
+  final GlobalKey<FormState> _formKey;
+  final Function onPressed;
+  final Color color;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          // TODO: Register here
+          this.onPressed();
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(this.text),
+      ),
+      style: ButtonStyle(
+        textStyle: MaterialStatePropertyAll<TextStyle>(
+          TextStyle(fontSize: 18.0),
+        ),
+        backgroundColor: MaterialStatePropertyAll<Color>(this.color),
       ),
     );
   }
